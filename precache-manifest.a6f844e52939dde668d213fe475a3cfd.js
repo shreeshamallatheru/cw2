2032e58fd7687027283a0cbbe30b9353996a2903
shreeshamallatheru <shreeshamallatheru@gmail.com>
2020-09-25 17:39:05 +0530

    adding files.

---
+++ precache-manifest.a6f844e52939dde668d213fe475a3cfd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6e7210996be2fa438ae889ae709866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6684384a8e4b3822588",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "4b537b1e61145c918143",
+    "url": "/static/css/main.fa08e655.chunk.css"
+  },
+  {
+    "revision": "e6684384a8e4b3822588",
+    "url": "/static/js/2.431c0533.chunk.js"
+  },
+  {
+    "revision": "264e16421001f5c9397c83c5a0263f9d",
+    "url": "/static/js/2.431c0533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b537b1e61145c918143",
+    "url": "/static/js/main.0eba502d.chunk.js"
+  },
+  {
+    "revision": "4f130009a0317e885fa5",
+    "url": "/static/js/runtime-main.e9877b13.js"
+  },
+  {
+    "revision": "c96db41d1b5c3faa139450e94b2d7dfa",
+    "url": "/static/media/bungalows.c96db41d.jpg"
+  },
+  {
+    "revision": "3166e3e02d883b41b2e636a8eef77d0a",
+    "url": "/static/media/cottage.3166e3e0.jpg"
+  },
+  {
+    "revision": "fc6b466da4dcf6118ae1637f7e088880",
+    "url": "/static/media/empty-lounges.fc6b466d.jpg"
+  },
+  {
+    "revision": "32f886364c9e014bbffdc5378f2591a9",
+    "url": "/static/media/person.32f88636.jpg"
+  },
+  {
+    "revision": "56b485bc8b82e0840fe12440bb8e7339",
+    "url": "/static/media/red-ham.56b485bc.jpg"
+  },
+  {
+    "revision": "88592725087f6945acca0e62bf003366",
+    "url": "/static/media/sand-bars.88592725.jpg"
+  }
+]);
